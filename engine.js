60d870d1cc05eaa567172371cf076adfb8ad5590
trevorcompton <39281697+trevorcompton@users.noreply.github.com>
2018-12-20 12:18:24 -0600

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,7 @@
-
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 36.174465, lng: -86.767960},
+          zoom: 8
+        });
+      }
